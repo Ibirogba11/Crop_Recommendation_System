ba71159fbc33525d02183d083a17c8466b3eb32d
Ibirogba11 <mathew.seun14@gmail.com>
2026-04-09 17:51:44 +0100

    Removed windows-specific libraries and updated port

--- app.py
+++ app.py
@@ -37,5 +37,7 @@ def predict():
     except Exception as e:
         return jsonify({'prediction': f"Error: {str(e)}"}), 400
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    # Get port from environment or default to 5000
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
